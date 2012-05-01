f0056fd62283bff41c6a2b20934bf731998448e6
skonowitch <supersam3@comcast.net>
2012-04-30 22:51:46 -0700

    generate team data

--- data/teams/generate_teams.py
+++ data/teams/generate_teams.py
@@ -6,17 +6,20 @@ def team(tid, name, hometown):
     
 f1 = open('mascots.txt')
 f2 = open('hometowns.txt')
+f3 = open('schools.txt')
 mascots = [line.strip() for line in f1 if line.strip() != ""]
 towns = [line.strip() for line in f2 if line.strip() != ""]
+schools = [line.strip() for line in f3 if line.strip() != ""]
 f1.close()
 f2.close()
+f3.close()
 
-for m in mascots:
-    mascot('%ss' % m, m)
+for i, (m, s) in enumerate(zip(mascots, schools)):
+    mascot('%s College' % s,'%ss'% m)
 
 c = 0
-while len(mascots) < 35:
-    mascots.append(mascots[c])
+while len(schools) < 35:
+    schools.append(schools[c])
     c += 1
 
 c = 0
@@ -24,8 +27,8 @@ while len(towns) < 35:
     towns.append(towns[c])
     c += 1
 
-for i, (m, t) in enumerate(zip(mascots, towns)):
-    team(i+1, '%ss'%m, t)
+for i, (s, t) in enumerate(zip(schools, towns)):
+    team(i+1, '%s College' % s, t)
     
      
     
