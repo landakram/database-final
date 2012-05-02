a2d344e3d9e842ab39f7df238604faadd419a54a
Mark Hudnall <hudnall.mark@gmail.com>
2012-05-02 14:20:21 -0700

    comment code, add to documentation, add all.sql

--- README.md
+++ README.md
@@ -6,19 +6,20 @@ Mark and Sam's awesome Database Systems final project.
 Installation
 ------------
 
-We assume that `mysql` and `python` are already installed.
+We assume that `mysql` and `>= python 2.6` are already installed on a OS X
+system. We tested our setup with both Lion and Snow Leopard.
 
 1. Get `pip`:
     
-        $ easy_install pip
+        $ sudo easy_install pip
 
 2. Get `virtualenv`:
 
-        $ pip install virtualenv
+        $ sudo pip install virtualenv
 
 3. Get `virtualenvwrapper`. It makes using `virtualenv` easier:
 
-        $ pip install virtualenvwrapper
+        $ sudo pip install virtualenvwrapper
 
 4. Add this to your `.bashrc` or run in your terminal:
 
@@ -33,12 +34,26 @@ you should see something like:
 
         (database-final) $ 
 
+If you don't, then you can enter a virtual environment by running:
+
+        $ workon database-final
+
+`database-final` is just the name of the virtualenv you created.
+
 7. To install the required dependencies for this project in one fell swoop,
 `cd` to the project root and then run:
 
         (database-final) $ pip install -r requirements.txt
 
-8. Now run the server:
+8. Now, let's populate our database. In the `data` folder, there 
+is a file called `all.sql`, which containts both the schema and 
+all generated data we used. In the project root:
+
+        (database-final) $ cd data
+        (database-final) $ mysql
+        mysql> source all.sql
+
+9. Now run the server:
 
         (database-final) $ python app.py
         * Running on http://127.0.0.1:5000/
@@ -46,5 +61,24 @@ you should see something like:
 The server should now be running on port 5000.
 
     
+Overview of Included Files
+--------------------------
+
+In the project root, we have a `README` with instructions for setting up the server. 
+The actual server code is contained in `app.py`. Settings for the server, including
+the database name, host, username, and password are in `settings.py`. 
+
+We also have several folders. `static` and `templates` are part of the server.
+`static` contains static resources like images, javascript code, and css. For our
+server, we used a UI framework called [Twitter Bootstrap](http://twitter.github.com/bootstrap/). 
+The `templates` folder contains various html templates used by our server. Flask
+templates are similar to JSPs, except that most of the application logic is done in `app.py`.
+Variables can be passed to templates and templates have limited control flow features like
+`for` loops and `if` statements. 
+
+The `data` folder contains various scripts and text files we used for generating
+our data. We have concatenated the result of running all the scripts into `all.sql`,
+but they are available for your perusal. The `docs` folder contains our final
+write-up, progress reports, and other documentation, like our ER diagram. 
 
     
--- app.py
+++ app.py
@@ -7,10 +7,16 @@ from functools import wraps
 
 from datetime import datetime
 
+# setup our Flask application
 app = Flask(__name__)
 app.config.from_object('settings')
 app.config['DEBUG'] = True
 
+# 
+# Athletics Organizer
+# Authors: Mark Hudnall and Sam Konowitch
+# Emails: mhh02009@mymail.pomona.edu, sk032009@mymail.pomona.edu
+# 
 
 # Decorator for view functions. Checks whether there is a user_id in the 
 # current session. If not, redirects to login page. If so, runs the view 
@@ -23,11 +29,14 @@ def login_required(f):
         return f(*args, **kwargs)
     return dec_fn
 
-
+# The index. If a user is not logged in, then we prompt them to log in or 
+# register. If they are logged in, we check their user type and redirect them
+# appropriately.
 @app.route('/')
 def index():
     # user is not logged in
     if not session.get('user_id'):
+        # open a cursor to the database
         cursor = g.db.cursor()
         cursor.execute("""SELECT T.tid, T.school, S.name FROM Team T, plays P, Sport S
                         WHERE T.tid = P.tid AND P.sid = S.sid 
@@ -41,7 +50,9 @@ def index():
     elif session['user_type'] == 'athlete':
         return redirect(url_for('athlete_info', uid=uid))
 
+### Coach views ###
 
+# Displays information about a coach including teams coached.
 @app.route('/coach/<int:uid>')
 @login_required
 def coach_info(uid):
@@ -51,19 +62,27 @@ def coach_info(uid):
                     WHERE C.uid=%s AND C.tid=T.tid""", (uid,))
     teams = cursor.fetchall()
 
+    # if we are requesting our own page, then we want to show them the salary
     if uid == session['user_id']:
         cursor.execute('SELECT salary FROM Coach WHERE uid = %s', (uid,))
         salary = cursor.fetchone()[0]
+    # otherwise, that is privileged information
     else:
         salary = None
     
     return render_template('coach.html', salary=salary, teams=teams, uid=uid)
 
 
+### Team views ###
+
+# Displays information about a team, like the team roster. If you are the 
+# coach of this team, then displays the recent workouts and allows you to 
+# create a new workout
 @app.route('/team/<int:tid>')
 @login_required
 def team_info(tid):
     cursor = g.db.cursor()
+    # general team information
     cursor.execute("""SELECT T.school, T.hometown, S.name 
                       FROM Team T, Sport S, plays P
                       WHERE T.tid = %s AND S.sid = P.sid  """, 
@@ -72,6 +91,8 @@ def team_info(tid):
     cursor.execute('SELECT mascot FROM TeamMascot WHERE school=%s',
                     (team[0],))
     mascot = cursor.fetchone()[0]
+
+    # get members of the team
     cursor.execute("""SELECT U.uid, M.number, U.name, M.position
                       FROM User U, member_of M
                       WHERE M.tid = %s AND M.uid = U.uid
@@ -79,8 +100,11 @@ def team_info(tid):
     members = cursor.fetchall()
 
     cursor.execute('SELECT uid FROM coaches WHERE tid = %s', (tid,)) 
+    # The cursor always returns tuples, so for a single value we just unpack
+    # it
     coaches = map(lambda (x,) : x, list(cursor.fetchall()))
 
+    # If multiple coaches, we want them all to be able to see the workouts
     current_coach = True if session['user_id'] in coaches else False
 
     cursor.execute("""SELECT wid, date_assigned 
@@ -98,12 +122,15 @@ def team_info(tid):
                                         workouts=workouts, mascot=mascot,
                                         tid=tid)
 
+# Allows a coach to create a workout for a given team.
 @app.route('/team/<int:tid>/workout/create')
 @login_required
 def create_workout(tid):
     cursor = g.db.cursor()
     cursor.execute('SELECT uid FROM coaches WHERE tid = %s AND uid=%s', 
                    (tid, session['user_id'])) 
+    # if the coach does not coach this team, we do not want them to be
+    # able to create workouts for them, so we redirect.
     if int(cursor.rowcount) == 0:
         flash('You don\'t have permission to do that', 'error')
         return redirect(url_for('team_info', tid=tid))
@@ -114,22 +141,28 @@ def create_workout(tid):
 
     return render_template('create.html', muscles=muscles, tid=tid)
 
+# Endpoint for workout form submission. This is actually called asynchronously
+# from the workout creation endpoint.
 @app.route('/team/<int:tid>/workout/submit', methods=['POST'])
 @login_required
 def submit_workout(tid):
     cursor = g.db.cursor()
     cursor.execute('SELECT uid FROM coaches WHERE tid = %s AND uid=%s', 
                    (tid, session['user_id'])) 
+    # if the coach does not coach this team, we do not want them to be
+    # able to create workouts for them, so we redirect.
     if int(cursor.rowcount) == 0:
         return jsonify(error='You don\'t have permission to create a workout for this team')
     
-    print request.form['workout[0][exercise]']
+    #print request.form['workout[0][exercise]']
 
     cursor.execute('INSERT INTO Workout(tid, date_assigned, uid) VALUES (%s, %s, %s)',
                     (tid, datetime.now(), session['user_id']))
     
+    # since Workout ids are auto incremented, we get the wid for the one we just created
     wid = cursor.lastrowid
 
+    # associate the exercises with the newly created workout
     for i in range(len(request.form)/3):
         cursor.execute('SELECT eid FROM Exercise WHERE name=%s',
                         (request.form['workout[%d][exercise]'%i]))
@@ -139,21 +172,27 @@ def submit_workout(tid):
         cursor.execute('INSERT INTO consists_of VALUES (%s, %s, %s, %s)',
                         (wid, eid, sets, reps))
 
+    # commit the transaction
     g.db.commit()
     return jsonify(success=True)
 
 
+# Displays the exercises in the workout. Also uses a stored procedure
+# to determine which members on the team have and have not completed the workout
 @app.route('/team/<int:tid>/workout/<int:wid>')
 @login_required
 def workout_info(tid, wid):
     cursor = g.db.cursor()
     cursor.execute('SELECT date_assigned FROM Workout WHERE wid=%s',(wid,))
     date = cursor.fetchall()[0][0]
+
+    # get the exercises associated with this workout
     cursor.execute("""SELECT E.eid, E.name, C.sets, C.reps
                       FROM Exercise E, consists_of C 
                       WHERE C.wid = %s AND E.eid = C.eid""", (wid,))
     exercises = cursor.fetchall()
 
+    # call the stored procedure. See schema.sql for details
     cursor.callproc("TeamProgress", (tid,wid))
     members = cursor.fetchall()
 
@@ -162,25 +201,36 @@ def workout_info(tid, wid):
                                            date=date,
                                            wid=wid)
 
+# This endpoint is used by the workout creation view. It is called 
+# asynchronously using ajax. Given a muscle group, it returns all exercise
+# names associated with that muscle group.
 @app.route('/exercises')
 def exercises():
     muscle = request.args.get('muscle')
     cursor = g.db.cursor()
+    # if they select any, then we just return all exercises
     if muscle == 'any':
         cursor.execute('SELECT name FROM ExerciseMuscles')
     else:
         cursor.execute('SELECT name FROM ExerciseMuscles WHERE muscle_group=%s',
                     (muscle,))
+    # unpack the tuple
     e = map(lambda (a,) : a, list(cursor.fetchall()))
+    # jsonify serializes the list and returns it with a json mimetype
     return jsonify(exercises=e)
 
+### Athlete views ###
 
+# Shows an athlete's performance for a given workout
 @app.route('/athlete/<int:uid>/workout/<int:wid>')
 @login_required
 def athlete_performance(uid, wid):
     cursor = g.db.cursor()
+    # get general info about the user
     cursor.execute('SELECT U.uid, U.name FROM User U WHERE U.uid = %s', (uid,))
     user = cursor.fetchone()
+    # if it's the current user, we make note of that. This variable is used
+    # in the template to decide whether to allow input
     if user[0] == session['user_id']:
         is_self = True
     else:
@@ -189,6 +239,7 @@ def athlete_performance(uid, wid):
     cursor.execute('SELECT date_assigned FROM Workout WHERE wid=%s',(wid,))
     date = cursor.fetchone()[0]
 
+    # essentially used to see whether the athlete has completed the workout
     cursor.execute('SELECT * FROM does D WHERE D.uid=%s AND D.wid=%s',
                     (uid,wid))
     # athlete has not completed workout
@@ -201,6 +252,7 @@ def athlete_performance(uid, wid):
         exercises = cursor.fetchall()
     else: 
         completed = True
+        # if completed, we query the performance table instead
         cursor.execute("""SELECT E.name, P.max_weight
                         FROM Exercise E, performance P
                         WHERE P.uid = %s AND P.wid = %s AND E.eid = P.eid""",
@@ -216,6 +268,7 @@ def athlete_performance(uid, wid):
                            wid=wid, is_self=is_self, completed=completed)
 
 
+# General information about an athlete
 @app.route('/athlete/<int:uid>')
 @login_required
 def athlete_info(uid):
@@ -231,6 +284,7 @@ def athlete_info(uid):
     teams = cursor.fetchall()
 
     workouts=[]
+    # get recent workouts
     for team in teams:
         tid=team[0]
 
@@ -250,6 +304,26 @@ def athlete_info(uid):
                             workouts=workouts)
 
 
+# Endpoint for performance form submission
+@app.route('/input/<int:wid>', methods=['POST'])
+@login_required
+def input(wid):
+    cursor = g.db.cursor()
+    uid = session['user_id']
+    cursor.execute('SELECT E.eid FROM consists_of E WHERE E.wid=%s',(wid,))
+    eids = cursor.fetchall()
+    # loops through form submission and inserts into performance table
+    for eid in eids:
+        reps=int(request.form['reps%s' % eid])
+        weight=int(request.form['max%s' % eid])
+        cursor.execute('INSERT INTO performance VALUES (%s, %s, %s, %s, %s)', (eid[0], uid, wid, reps, weight))
+    cursor.execute('INSERT INTO does VALUES (%s, %s, %s)', (wid,uid,datetime.now()))
+    g.db.commit()    
+    return redirect(url_for('athlete_performance',uid=uid,wid=wid))
+
+### General User Views ###
+
+# Endpoint for login form submission
 @app.route('/login', methods=['POST'])
 def login():
     email = request.form['email'] 
@@ -277,8 +351,10 @@ def login():
     return redirect(url_for('index'))
 
 
+# Registers a user
 @app.route('/register', methods=['POST'])
 def register():
+    # Grab the information from the request body
     name = request.form['name']
     email = request.form['email']
     password = request.form['password']
@@ -293,6 +369,8 @@ def register():
         return redirect(url_for('index'))
 
     uid = 0
+    # if the user is an athlete, then we insert them into the Athletes table
+    # and put them on a team, as well as insert them into the User table
     if user_type == 'athlete':
         weight = float(request.form['weight'])
         height = float(request.form['height'])
@@ -304,6 +382,8 @@ def register():
                     (uid, height, weight))
         cursor.execute('INSERT INTO member_of VALUES(%s, %s, "Bench", NextTeamNumber(%s))',
                         (uid, tid, tid))
+    # if the user is a coach, then we insert them into the Coach table
+    # and have them coach a team, as well as insert them into the User table
     elif user_type == 'coach':
         salary = float(request.form['salary'])
 
@@ -313,29 +393,19 @@ def register():
         cursor.execute('INSERT INTO Coach VALUES(%s, %s)', (uid, salary))
         cursor.execute('INSERT INTO coaches VALUES(%s, %s, %s)',
                         (uid, tid, datetime.now()))
+    # commit the transaction
     g.db.commit()
 
+    # set the cookies
     session['user_id'] = uid
     session['user_name'] = name
     session['user_type'] = user_type
     flash('You were successfully registered and logged in.', 'success')
     return redirect(url_for('index'))
 
-@app.route('/input/<int:wid>', methods=['POST'])
-def input(wid):
-    cursor = g.db.cursor()
-    uid=session['user_id']
-    cursor.execute('SELECT E.eid FROM consists_of E WHERE E.wid=%s',(wid,))
-    eids = cursor.fetchall()
-    for eid in eids:
-        reps=int(request.form['reps%s' % eid])
-        weight=int(request.form['max%s' % eid])
-        cursor.execute('INSERT INTO performance VALUES (%s, %s, %s, %s, %s)', (eid[0], uid, wid, reps, weight))
-    cursor.execute('INSERT INTO does VALUES (%s, %s, %s)', (wid,uid,datetime.now()))
-    g.db.commit()    
-    return redirect(url_for('athlete_performance',uid=uid,wid=wid))
 
-
+# Logs a user out. We do this by deleting the session cookies that contain 
+# currently logged in users information.
 @app.route('/logout')
 def logout():
     try:
@@ -347,7 +417,9 @@ def logout():
     finally:
         return redirect(url_for('index'))
 
-
+# This function is run before every request. It connects to the database
+# and stores the connection on the "g" object, which is available for
+# the lifetime of a request.
 @app.before_request
 def before_request():
     g.db = mysql.connect(host = app.config['DB_HOST'], 
@@ -355,7 +427,7 @@ def before_request():
                          user = app.config['DB_USER'],
                          passwd = app.config['DB_PASSWORD'])
 
-
+# Closes the database connection after a request finishes
 @app.teardown_request
 def teardown_request(exception):
     g.db.close()
